a5bdab44366f9679c34a509e55f55bfc50f2381a
melisatirabassi <124107756+melisatirabassi@users.noreply.github.com>
2023-05-15 14:56:20 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,56 @@
+from fastapi import FastAPI
+import pandas as pd
+import numpy as np
+import joblib
+import requests
+import io
+
+
+#df_preliminar = pd.read_csv('/Users/melisatirabassi/Library/CloudStorage/OneDrive-Personal/Documentos/Diplomatura Data Science/Henry/dataset_ml.csv')
+#similarity_matrix = joblib.load('modelo.joblib')
+
+def download_file_from_dropbox(access_token, file_path):
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "Dropbox-API-Arg": f'{{"path": "{file_path}"}}'
+    }
+    try:
+        response = requests.post(
+            "https://content.dropboxapi.com/2/files/download",
+            headers=headers
+        )
+        response.raise_for_status()
+        file_data = response.content
+        return file_data
+    except requests.exceptions.RequestException as e:
+        print(f"Error downloading file from Dropbox: {e}")
+
+
+# Ejemplo de uso
+access_token = "sl.BedMZt7YTRmWz6TqiLk0FbYMSN2wlybUBmHCI0zT3FeIq5E-rIyCdtamVUzTRjLx2jAoKTg4fxD7eXhuGTmYCCQLIGVQfa7OaqHs9t7tG9Aupjn3vIkwt4gWFlI0Y4LjYU_kdcIt"
+file_path = "/dataset_ml.csv"  # Ruta del archivo en Dropbox
+file_path_model = "/modelo.joblib"  # Ruta del archivo en Dropbox
+file_data_df = download_file_from_dropbox(access_token, file_path)
+file_data_model = download_file_from_dropbox(access_token, file_path_model)
+# Procesa el archivo de datos según tus necesidades
+
+df_preliminar= pd.read_csv(io.BytesIO(file_data_df))
+similarity_matrix = joblib.load(io.BytesIO(file_data_model))
+
+app = FastAPI()
+
+@app.post("/peliculas_recomendadas")
+def predict(data: str):
+    # Make predictions using the loaded model
+    input_movie_index = df_preliminar.index[df_preliminar['title']==data]
+    movie_scores = similarity_matrix[input_movie_index]
+    similar_movie_indices = np.argsort(movie_scores)
+    similar_movie_indices = similar_movie_indices[-1][::-1]
+    similar_movie_indices = similar_movie_indices[:6]
+    recommended_movies = []
+    for index in similar_movie_indices:
+        if index < len(df_preliminar):
+            recommended_movies.append(df_preliminar.iloc[index]['title'])
+    # Return the predictions as the API response
+    return {"predictions": recommended_movies}
+
